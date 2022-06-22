e702a4b307acfe375db7f60dc2c8c3968087deb8
Igor Zharov <stawr.900@gmail.com>
2022-06-22 12:12:25 +0300

    js_modal_v2

--- js/script.js
+++ js/script.js
@@ -94,20 +94,23 @@ window.addEventListener('DOMContentLoaded', function() {
         modalCloseBtn = document.querySelector('[data-close]'),
         modal = document.querySelector('.modal');
 
-    modalTrigger.forEach(btn => { 
-        btn.addEventListener('click', () => {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
 
-            document.body.style.overflow = 'hidden'; 
-        });
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
     });
 
     function closeModal() {
         modal.classList.add('hide');
         modal.classList.remove('show');
 
-        document.body.style.overflow = ''; 
+        document.body.style.overflow = '';
     }
 
     modalCloseBtn.addEventListener('click', closeModal);
@@ -123,4 +126,15 @@ window.addEventListener('DOMContentLoaded', function() {
             closeModal();
         }
     });
+
+    const modalTimerId = setTimeout(openModal, 15000);
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll); 
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });
